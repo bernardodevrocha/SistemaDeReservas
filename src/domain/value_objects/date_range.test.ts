eaa8e2e82efbf81ae3242d470ec35eed3d7557eb
bernardodevrocha <programadorbernardords@gmail.com>
2025-08-15 17:14:30 -0400

    Testing total nights users need

---
+++ src/domain/value_objects/date_range.test.ts
@@ -0,0 +1,27 @@
+import { DateRange } from "./date_range";
+
+describe("DateRange value object", () => {
+  it("Deve lançar um erro se a data de término for antes da data de início.", () => {
+    expect(() => {
+      new DateRange(new Date("2025-12-25"), new Date("2025-12-20"));
+    }).toThrow("A data de término deve ser posterior a data de início")
+  });
+
+  it("Deve criar uma instância de DateRange com a data de início e data de término", () => {
+    const startDate = new Date("2025-12-20");
+    const endDate = new Date("2025-12-25");
+    const dateRange = new DateRange(startDate, endDate);
+    expect(dateRange.getStartDate()).toEqual(startDate);
+    expect(dateRange.getEndDate()).toEqual(endDate);
+  });
+
+  it("Deve calcular o total de noites corretamente", () => {
+    const startDate = new Date("2025-12-20");
+    const endDate = new Date("2025-12-25");
+    const dateRange = new DateRange(startDate, endDate);
+    
+    const totalNights = dateRange.getTotalNights();
+    
+    expect(totalNights).toBe(5);
+  })
+});
